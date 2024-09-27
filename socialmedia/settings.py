c725b9101aa37f36a08186cfb3832943bdaa28e8
FrankNeba <ngwafranklin29@gmail.com>
2024-09-27 16:50:43 +0100

    email verifictaion added

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -148,6 +148,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_MAIL = "Socia"
+DEFAULT_FROM_MAIL = "ngwafranklin29@gmail.com"
 EMAIL_HOST_USER ="ngwafranklin29@gmail.com"
 EMAIL_HOST_PASSWORD = "lgxw jovy uayr ockb"
