48479c5e8f9c176beb0b3512d2bb6df216878489
FrankNeba <ngwafranklin29@gmail.com>
2024-10-15 09:16:32 +0100

    message sent fixed

--- socialmedia/settings.py
+++ socialmedia/settings.py
@@ -99,9 +99,9 @@ CHANNEL_LAYERS = {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             # "hosts": [('redis://red-cs59vfdumphs73aqsv10:6379')],
-            "hosts": [('redis://default:ZQaUgMrZXyTEoZbGWbaQChfuXyZiHWIP@junction.proxy.rlwy.net:25326')],
+            # "hosts": [('redis://default:ZQaUgMrZXyTEoZbGWbaQChfuXyZiHWIP@junction.proxy.rlwy.net:25326')],
             # 'hosts': [('redis://localhost:6379/0')]
-            # 'hosts': [('redis://default:ZQaUgMrZXyTEoZbGWbaQChfuXyZiHWIP@redis-fxhu.railway.internal:6379')]
+            'hosts': [('redis://default:ZQaUgMrZXyTEoZbGWbaQChfuXyZiHWIP@redis-fxhu.railway.internal:6379')]
         },
     },
 }
